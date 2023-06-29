4ef4cb2fe3634236b2d1e2ed65c9005f6db4f53f
dmc/dev <celsinodel13@gmail.com>
2023-06-29 19:36:35 +0200

    Tests de la modale, fonction ok mais bug affichage

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -1,6 +1,7 @@
 /****** API ******/
 
 // Load de l'API
+
 async function loadApi() {
     await fetch('http://localhost:5678/api/works')
         .then(response => response.json())
@@ -15,6 +16,7 @@ loadApi();
 /****** Création du DOM ******/
 
 // Création de la gallery de projets
+
 function createGallery() {
     for (let i = 0; i < works.length; i++) {
         const sectionWorks = document.querySelector('.gallery');
@@ -32,6 +34,7 @@ function createGallery() {
 }
 
 // Création des différents filtres
+
 async function loadFiltres() {
     await fetch('http://localhost:5678/api/categories')
         .then(response => response.json())
@@ -52,6 +55,7 @@ async function loadFiltres() {
 }
 
 // Activation du mode Admin
+
 if (localStorage.getItem('token')) {
     async function createAdminMode() {
         const editButtonDiv = document.querySelector('.portfolio-title');
@@ -72,6 +76,9 @@ if (localStorage.getItem('token')) {
         editButtonDiv.appendChild(editButton);
         loginButtonDiv.appendChild(loginButton);
 
+        // Ajout de l'écouteur d'événement pour ouvrir la modale
+        editButton.addEventListener('click', openModal);
+
         loginButton.addEventListener('click', function () {
             localStorage.removeItem('token');
             window.location.href = 'index.html';
@@ -91,3 +98,57 @@ if (localStorage.getItem('token')) {
     loginButton.appendChild(loginButtonLink);
     loginButtondiv.appendChild(loginButton);
 }
+
+// Déclarez une variable pour stocker la référence de la modale
+let modal = null;
+
+// Fonction pour créer et afficher la modale
+function openModal() {
+    if (localStorage.getItem('token')) {
+        // Vérifiez si la modale existe déjà
+        if (!modal) {
+            modal = createModal();
+            document.body.appendChild(modal);
+        }
+        console.log("La fonction openModal() a été appelée !");
+    }
+}
+
+// Fonction pour fermer la modale
+
+function closeModal() {
+    // Vérifiez si la modale existe avant de la supprimer
+
+    if (modal) {
+        modal.remove();
+        modal = null;
+    }
+}
+
+// Fonction pour créer la structure HTML de la modale
+
+function createModal() {
+    const modal = document.createElement('div');
+    modal.classList.add('modal');
+
+    const content = document.createElement('div');
+    content.classList.add('modal-content');
+
+    const closeButton = document.createElement('span');
+    closeButton.classList.add('close-button');
+    closeButton.innerHTML = '&times;';
+    closeButton.addEventListener('click', closeModal);
+
+    content.appendChild(closeButton);
+    modal.appendChild(content);
+
+    return modal;
+}
+
+// Sélectionnez l'élément du bouton "Modifier"
+
+const editButton = document.querySelector('.portfolio-title button');
+
+// Ajoutez l'écouteur d'événement pour ouvrir la modale lorsque le bouton est cliqué
+
+editButton.addEventListener('click', openModal);
